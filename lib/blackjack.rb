c5432e8e068c077e84c4e721b141bf9a259e60ff
Colton Kaiser <kaiserc2@seattleu.edu>
2020-04-16 23:17:25 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,10 +57,10 @@ def runner
   welcome
   card_total = initial_round
   hit?(card_total)
-  binding.pry
   display_card_total(card_total)
   until card_total > 21
     card_total += hit?(card_total)
+    binding.pry
     display_card_total(card_total)
 end
 end_game(card_total)
